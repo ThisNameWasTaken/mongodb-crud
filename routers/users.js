350145bf8ff84b20a44017823d66d8d2b4179262
Adrian <adriandinca2008@yahoo.com>
2019-03-06 17:19:41 +0200

    refactor: Use the express-async-erros package

--- routers/users.js
+++ routers/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
+require('express-async-errors');
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
 
 // Connection URL
 const url = 'mongodb://localhost:27017';
@@ -22,110 +22,95 @@ const DB_NAME = 'test-db';
 //   );
 // });
 
-const _ = fn => (req, res, next, ...args) =>
-  fn(req, res, next, ...args).catch(next);
+router.post('/log-in', async (req, res) => {
+  const userData = req.body;
+  const { email } = userData;
 
-router.post(
-  '/log-in',
-  _(async (req, res) => {
-    const userData = req.body;
-    const { email } = userData;
+  MongoClient.connect(url, async (err, client) => {
+    if (err) throw err;
 
-    MongoClient.connect(url, async (err, client) => {
-      if (err) throw err;
+    const db = client.db(DB_NAME);
 
-      const db = client.db(DB_NAME);
+    const userCollection = db.collection('users');
 
-      const userCollection = db.collection('users');
+    const userEntry = await userCollection.findOne({ email });
+    if (userEntry) {
+      res.send('logged in');
+    } else {
+      res.send('must register first');
+    }
 
-      const userEntry = await userCollection.findOne({ email });
-      if (userEntry) {
-        res.send('logged in');
-      } else {
-        res.send('must register first');
-      }
+    client.close();
+  });
+});
 
-      client.close();
-    });
-  })
-);
+router.post('/register', async (req, res) => {
+  const userData = req.body;
+  const { email } = userData;
 
-router.post(
-  '/register',
-  _(async (req, res) => {
-    const userData = req.body;
-    const { email } = userData;
+  MongoClient.connect(url, async (err, client) => {
+    if (err) throw err;
 
-    MongoClient.connect(url, async (err, client) => {
-      if (err) throw err;
+    const db = client.db(DB_NAME);
 
-      const db = client.db(DB_NAME);
+    const userCollection = db.collection('users');
 
-      const userCollection = db.collection('users');
+    const userEntry = await userCollection.findOne({ email });
+    if (userEntry) {
+      res.send('already registered');
+    } else {
+      await userCollection.insertOne(userData);
+      res.send(`${email} has been registered`);
+    }
 
-      const userEntry = await userCollection.findOne({ email });
-      if (userEntry) {
-        res.send('already registered');
-      } else {
-        await userCollection.insertOne(userData);
-        res.send(`${email} has been registered`);
-      }
+    client.close();
+  });
+});
 
-      client.close();
-    });
-  })
-);
+router.put('/update-info', async (req, res) => {
+  const userData = req.body;
+  const { email } = userData;
 
-router.put(
-  '/update-info',
-  _(async (req, res) => {
-    const userData = req.body;
-    const { email } = userData;
+  MongoClient.connect(url, async (err, client) => {
+    if (err) throw err;
 
-    MongoClient.connect(url, async (err, client) => {
-      if (err) throw err;
+    const db = client.db(DB_NAME);
 
-      const db = client.db(DB_NAME);
+    const userCollection = db.collection('users');
 
-      const userCollection = db.collection('users');
+    const userEntry = await userCollection.findOne({ email });
+    if (userEntry) {
+      await userCollection.updateOne({ email }, { $set: { ...userData } });
+      res.send(`${email} has been updated`);
+    } else {
+      res.send('must register first');
+    }
 
-      const userEntry = await userCollection.findOne({ email });
-      if (userEntry) {
-        await userCollection.updateOne({ email }, { $set: { ...userData } });
-        res.send(`${email} has been updated`);
-      } else {
-        res.send('must register first');
-      }
+    client.close();
+  });
+});
 
-      client.close();
-    });
-  })
-);
+router.delete('/delete-account', async (req, res) => {
+  const userData = req.body;
+  const { email } = userData;
 
-router.delete(
-  '/delete-account',
-  _(async (req, res) => {
-    const userData = req.body;
-    const { email } = userData;
+  MongoClient.connect(url, async (err, client) => {
+    if (err) throw err;
 
-    MongoClient.connect(url, async (err, client) => {
-      if (err) throw err;
+    const db = client.db(DB_NAME);
 
-      const db = client.db(DB_NAME);
+    const userCollection = db.collection('users');
 
-      const userCollection = db.collection('users');
+    const userEntry = await userCollection.findOne({ email });
+    if (userEntry) {
+      await userCollection.deleteOne({ email });
+      res.send(`${email} has been removed`);
+    } else {
+      res.send('user not registered');
+    }
 
-      const userEntry = await userCollection.findOne({ email });
-      if (userEntry) {
-        await userCollection.deleteOne({ email });
-        res.send(`${email} has been removed`);
-      } else {
-        res.send('user not registered');
-      }
-
-      client.close();
-    });
-  })
-);
+    client.close();
+  });
+});
 
 module.exports = router;
